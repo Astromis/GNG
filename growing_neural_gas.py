406d72e1c4aa80d0f50f7cf0e447bc45cc5ed675
Artiom N <artiom14@yandex.ru>
2018-06-09 22:20:04 +0300

    Initial IGNG implementation

--- growing_neural_gas.py
+++ growing_neural_gas.py
@@ -7,6 +7,7 @@ import operator
 import imageio
 from collections import OrderedDict
 from scipy.spatial.distance import euclidean
+from sklearn.metrics import calinski_harabaz_score
 from sklearn import preprocessing
 import csv
 import numpy as np
@@ -27,27 +28,6 @@ def sh(s):
     return sum
 
 
-def shrink_to_3d(data):
-    result = []
-    
-    for i in data:
-        depth = len(i)
-        if depth <=3:
-            result.append(i)
-        else:
-            sm = np.sum([(n) * v for n, v in enumerate(i[2:])])
-            if sm == 0:
-                sm = 1
-
-            r = np.array([i[0], i[1], i[2]])
-            r *= sm
-            r /= np.sum(r)
-
-            result.append(r)
-            
-    return preprocessing.normalize(result)
-
-
 def read_test_file():
     """Read the file and return the indices as list of lists."""
     filename = 'test_data.txt'
@@ -60,8 +40,6 @@ def read_test_file():
 def create_test_data_graph(array2d):
     """Create the graph and returns the networkx version of it 'G'."""
 
-    pos = None
-
     row, column = len(array2d), len(array2d[0])
     count = 0
 
@@ -73,8 +51,6 @@ def create_test_data_graph(array2d):
                 G.add_node(count, pos=(j, i, np.random.randint(-1, 1)))
                 count += 1
 
-    pos = nx.get_node_attributes(G, 'pos')
-
     """
     for index in pos.keys():
         for index2 in pos.keys():
@@ -83,13 +59,12 @@ def create_test_data_graph(array2d):
             if pos[index][1] == pos[index2][1] and pos[index][0] == pos[index2][0] - 1:
                 G.add_edge(index, index2, weight=1)
     """
-    return G, pos
+    return G
 
 
 def create_data_graph(dots):
     """Create the graph and returns the networkx version of it 'G'."""
 
-    pos = None
     count = 0
 
     G = nx.Graph()
@@ -98,9 +73,7 @@ def create_data_graph(dots):
         G.add_node(count, pos=(i))
         count += 1
 
-    pos = nx.get_node_attributes(G, 'pos')
-
-    return G, pos
+    return G
 
 
 def get_ra(ra=0, ra_step=0.3):
@@ -110,6 +83,27 @@ def get_ra(ra=0, ra_step=0.3):
         else:
             ra += ra_step
         yield ra
+
+
+def shrink_to_3d(data):
+    result = []
+
+    for i in data:
+        depth = len(i)
+        if depth <= 3:
+            result.append(i)
+        else:
+            sm = np.sum([(n) * v for n, v in enumerate(i[2:])])
+            if sm == 0:
+                sm = 1
+
+            r = np.array([i[0], i[1], i[2]])
+            r *= sm
+            r /= np.sum(r)
+
+            result.append(r)
+
+    return preprocessing.normalize(result)
 
 
 def draw_graph3d(graph, fignum, clear=True,
@@ -127,10 +121,10 @@ def draw_graph3d(graph, fignum, clear=True,
     graph_pos = nx.get_node_attributes(graph, 'pos')
     
     # numpy array of x, y, z positions in sorted node order
-    xyz = shrink_to_3d(np.array([graph_pos[v] for v in sorted(gr)]))
+    xyz = shrink_to_3d(np.array([graph_pos[v] for v in sorted(gr)], dtype='float32'))
 
     # scalar colors
-    scalars = np.array([n for n in gr.nodes()])
+    #scalars = np.array([n for n in gr.nodes()])
     
     if mlab.options.offscreen:
         mlab.figure(fignum, bgcolor=bgcolor, fgcolor=text_color, size=size)
@@ -143,8 +137,9 @@ def draw_graph3d(graph, fignum, clear=True,
 
     # the x,y, and z co-ordinates are here
     # manipulate them to obtain the desired projection perspective 
-    pts = mlab.points3d(xyz[:,0], xyz[:,1], xyz[:,2],
-                        #scalars,
+
+    pts = mlab.points3d(xyz[:, 0], xyz[:, 1], xyz[:, 2],
+                        # scalars,
                         scale_factor=node_size,
                         scale_mode='none',
                         color=node_color,
@@ -234,11 +229,11 @@ def read_ids_data(data_file, is_normal=True, labels_file='NSL_KDD/Field Names.cs
     return result
 
 
-class GNG():
-    """Growing Neural Gas multidimensional implementation"""
+class IGNG():
+    """Incremental Growing Neural Gas multidimensional implementation"""
 
     def __init__(self, data, surface_graph=None, eps_b=0.2, eps_n=0.006, max_age=50,
-                 lambda_=100, alpha=0.5, d=0.995, max_nodes=1000,
+                 lambda_=100, a_mature=1, d=0.995, max_nodes=100,
                  output_images_dir='images'):
         """."""
         self.graph = nx.Graph()
@@ -247,7 +242,7 @@ class GNG():
         self.eps_n = eps_n
         self.max_age = max_age
         self.lambda_ = lambda_
-        self.alpha = alpha
+        self.a_mature = a_mature
         self.d = d
         self.max_nodes = max_nodes
         self.num_of_input_signals = 0
@@ -257,23 +252,10 @@ class GNG():
         }
         self._surface_graph = surface_graph
 
-        self._pos = None
         self._fignum = 0
-
-        node1 = data[np.random.randint(0, len(data))]
-        node2 = data[np.random.randint(0, len(data))]
-
-        # make sure you dont select same positions
-        if self.is_nodes_equal(node1, node2):
-            print("Rerun ---------------> similar nodes selected")
-            return None
 
         # initialize here
         self.count = 0
-        self.graph.add_node(self.count, pos=node1, error=0)
-        self.count += 1
-        self.graph.add_node(self.count, pos=node2, error=0)
-        self.graph.add_edge(self.count - 1, self.count, age=0)
         
         self._output_images_dir = output_images_dir
 
@@ -283,6 +265,37 @@ class GNG():
         os.makedirs(output_images_dir)
 
         print("Ouput images will be saved in: {0}".format(output_images_dir))
+
+    def calinski_harabaz_score(self):
+        return
+        #calinski_harabaz_score(self.data, np.array([graph_pos[v] for v in sorted(self.graph)], dtype='float32'))
+        extra_disp, intra_disp = 0., 0.
+
+        # CHI = [B / (c - 1)]/[W / (n - c)]
+        # Total numb er of neurons.
+        c = self.count
+        # Total number of data.
+        n = len(self.data)
+        
+        # Mean of the all data.
+        mean = np.mean(self.data, axis=0)
+        
+        # Neuron reference vector.
+        nrv = np.array([graph_pos[v] for v in sorted(self.graph)], dtype='float32')
+        
+        print(mean, c, n, nrv)
+        print(mean - nrv)
+        exit(0)
+
+        for k in range(n_labels):
+            cluster_k = X[labels == k]
+            mean_k = np.mean(cluster_k, axis=0)
+            extra_disp += len(cluster_k) * np.sum((mean_k - mean) ** 2)
+            intra_disp += np.sum((cluster_k - mean_k) ** 2)
+
+        return (1. if intra_disp == 0. else
+                extra_disp * (n_samples - n_labels) /
+                (intra_disp * (n_labels - 1.)))
 
     def number_of_clusters(self):
         return nx.number_connected_components(self.graph)
@@ -300,88 +313,77 @@ class GNG():
     def determine_2closest_vertices(self, curnode):
         """Where this curnode is actually the x,y index of the data we want to analyze."""
         #python growing_neural_gas.py  320,47s user 1,33s system 99% cpu 5:22,95 total
-        pos = self._pos = nx.get_node_attributes(self.graph, 'pos')
+        pos = nx.get_node_attributes(self.graph, 'pos')
 
-        winner1 = [0, sys.maxint]
-        winner2 = [0, sys.maxint]
+        winner1 = None
+        winner2 = None
 
         for node, position in iteritems(pos):
             dist = self.distance(curnode, position)
-            if dist < winner1[1]:
+            if winner1 is None or dist < winner1[1]:
                 winner1 = [node, dist]
-            if node != winner1[0] and dist < winner2[1]:
+                continue
+            if winner2 is None or dist < winner2[1]:
                 winner2 = [node, dist]
 
         return winner1, winner2
 
-    def get_new_position(self, winnerpos, nodepos):
-        """."""
-
-        move_delta = tuple(self.eps_b * (i - j) for i, j in zip(nodepos, winnerpos))
-        newpos = tuple(i + j for i, j in zip(move_delta, winnerpos))
-
-        return newpos
-
-    def get_new_position_neighbors(self, neighborpos, nodepos):
-        """."""
-
-        movement = tuple(self.eps_n * (i - j) for i, j in zip(nodepos, neighborpos))
-        newpos = tuple(i + j for i, j in zip(neighborpos, movement))
-
-        return newpos
-
-    def update_winner(self, curnode):
+    def update_winner(self, cur_node):
         """."""
 
         # find nearest unit and second nearest unit
-        winner1, winner2 = self.determine_2closest_vertices(curnode)
-        winnernode = winner1[0]
-        winnernode2 = winner2[0]
-        win_dist_from_node = winner1[1]
+        winner1, winner2 = self.determine_2closest_vertices(cur_node)
         graph = self.graph
 
-        errorvectors = nx.get_node_attributes(self.graph, 'error')
-
-        error1 = errorvectors[winner1[0]]
-        # update the new error
-        newerror = error1 + win_dist_from_node**2
-        graph.add_node(winnernode, error=newerror)
-
-        # move the winner node towards current node
-        self._pos = nx.get_node_attributes(graph, 'pos')
-        newposition = self.get_new_position(self._pos[winnernode], curnode)
-        graph.add_node(winnernode, pos=newposition)
-
-        # now update all the neighbors distances and their ages
-        neighbors = nx.all_neighbors(graph, winnernode)
-        age_of_edges = nx.get_edge_attributes(graph, 'age')
-        for n in neighbors:
-            newposition = self.get_new_position_neighbors(self._pos[n], curnode)
-            graph.add_node(n, pos=newposition)
-            key = (int(winnernode), n)
-            if key in age_of_edges:
-                newage = age_of_edges[(int(winnernode), n)] + 1
-            else:
-                newage = age_of_edges[(n, int(winnernode))] + 1
-            graph.add_edge(winnernode, n, age=newage)
-
-        # no sense in what I am writing here, but with algorithm it goes perfect
-        # if winnner and 2nd winner are connected, update their age to zero
-        """
-        if (self.graph.get_edge_data(winnernode, winnernode2) is not None):
-            self.graph.add_edge(winnernode, winnernode2, age=0)
+        if winner1 is None or winner1[1] >= self.d:
+            # 0 - is an embryo type.
+            graph.add_node(self.count, pos=cur_node, error=0, n_type=0, age=0)
+            winner_node1 = self.count
+            self.count += 1
+            return
         else:
-            # else create an edge between them
-            self.graph.add_edge(winnernode, winnernode2, age=0)
-        """
-        # Optimized code.
-        graph.add_edge(winnernode, winnernode2, age=0)
+            winner_node1 = winner1[0]
 
-        # if there are ages more than maximum allowed age, remove them
+        if winner2 is None or winner2[1] >= self.d:
+            # 0 - is an embryo type.
+            graph.add_node(self.count, pos=cur_node, error=0, n_type=0, age=0)
+            winner_node2 = self.count
+            self.count += 1
+            graph.add_edge(winner_node1, winner_node2, age=0)
+            return
+        else:
+            winner_node2 = winner2[0]
+
+        # Increment the age of all edges, emanating from the winner.
+        for e in graph.edges(winner_node1, data=True):
+            e[2]['age'] += 1
+
+        w_node = graph.nodes[winner_node1]
+        # Move the winner node towards current node.
+        w_node['pos'] += self.eps_b * (cur_node - w_node['pos']) #self.get_new_position(w_node['pos'], cur_node)
+
+        neighbors = nx.all_neighbors(graph, winner_node1)
+        a_mature = self.a_mature
+
+        for n in neighbors:
+            c_node = graph.nodes[n]
+            # Move all direct neighbors of the winner.
+            c_node['pos'] += self.eps_n * (cur_node - c_node['pos'])
+            # Increment the age of all direct neighbors of the winner.
+            c_node['age'] += 1
+            if c_node['n_type'] == 0 and c_node['age'] >= a_mature:
+                # Now, it's a mature neuron.
+                c_node['n_type'] = 1
+
+        # Create connection with age == 0 between two winners.
+        graph.add_edge(winner_node1, winner_node2, age=0)
+
+        max_age = self.max_age
+
+        # If there are ages more than maximum allowed age, remove them.
         age_of_edges = nx.get_edge_attributes(graph, 'age')
         for edge, age in iteritems(age_of_edges):
-
-            if age > self.max_age:
+            if age >= max_age:
                 #!!!
                 graph.remove_edge(edge[0], edge[1])
 
@@ -402,74 +404,27 @@ class GNG():
 
         if self._surface_graph is not None:
             draw_graph3d(self._surface_graph, fignum)
-         
+        
+        if not fignum:
+            return
+
         draw_graph3d(self.graph, fignum, clear=False, node_color=(1, 0, 0))
         mlab.savefig("{0}/{1}.png".format(self._output_images_dir, str(fignum)))
 
-    def train(self, max_iterations=10000):
+    def train(self, max_iterations=100):
         """."""
 
         fignum = self._fignum
-
         self.save_img(fignum)
 
         for i in xrange(1, max_iterations):
-            print("Iterating..{0:d}/{1}".format(i, max_iterations))
+            print('CHS', self.calinski_harabaz_score())
+            print('Iterating..{0:d}/{1}'.format(i, max_iterations))
             for x in self.data:
                 self.update_winner(x)
-
-                # step 8: if number of input signals generated so far
-                if i % self.lambda_ == 0 and len(self.graph.nodes()) <= self.max_nodes:
-                    # find a node with the largest error
-                    errorvectors = nx.get_node_attributes(self.graph, 'error')
-                    node_largest_error = max(iteritems(errorvectors), key=operator.itemgetter(1))[0]
-
-                    # find a node from neighbor of the node just found,
-                    # with largest error
-                    neighbors = self.graph.neighbors(node_largest_error)
-                    max_error_neighbor = None
-                    max_error = -1
-                    errorvectors = nx.get_node_attributes(self.graph, 'error')
-                    for n in neighbors:
-                        if errorvectors[n] > max_error:
-                            max_error = errorvectors[n]
-                            max_error_neighbor = n
-
-                    # insert a new unit half way between these two
-                    self._pos = nx.get_node_attributes(self.graph, 'pos')
-
-                    newnodepos = self.get_average_dist(self._pos[node_largest_error], self._pos[max_error_neighbor])
-                    self.count = self.count + 1
-                    newnode = self.count
-                    self.graph.add_node(newnode, pos=newnodepos)
-
-                    # insert edges between new node and other two nodes
-                    self.graph.add_edge(newnode, max_error_neighbor, age=0)
-                    self.graph.add_edge(newnode, node_largest_error, age=0)
-
-                    # remove edge between the other two nodes
-
-                    self.graph.remove_edge(max_error_neighbor, node_largest_error)
-
-                    # decrease error variable of other two nodes by multiplying with alpha
-                    errorvectors = nx.get_node_attributes(self.graph, 'error')
-                    error_max_node = self.alpha * errorvectors[node_largest_error]
-                    error_max_second = self.alpha * max_error
-                    self.graph.add_node(max_error_neighbor, error=error_max_second)
-                    self.graph.add_node(node_largest_error, error=error_max_node)
-
-                    # initialize the error variable of newnode with max_node
-                    self.graph.add_node(newnode, error=error_max_node)
-
-                    fignum += 1
-                    self.save_img(fignum)
-
-                # step 9: Decrease all error variables
-                errorvectors = nx.get_node_attributes(self.graph, 'error')
-                for i in self.graph.nodes():
-                    olderror = errorvectors[i]
-                    newerror = olderror - self.d * olderror
-                    self.graph.add_node(i, error=newerror)
+            self.d -= 0.1 * self.d
+            fignum += 1
+            self.save_img(fignum)
 
         self._fignum = fignum
 
@@ -507,24 +462,24 @@ def main():
     mlab.options.offscreen = True
     output_images_dir = 'images'
 
-    #G, pos = create_test_data_graph(read_test_file())
+    #G = create_test_data_graph(read_test_file())
     data = read_ids_data('NSL_KDD/Small Training Set.csv')
     #data = read_ids_data('NSL_KDD/20 Percent Training Set.csv')
     #data = read_ids_data('NSL_KDD/KDDTrain+.txt')
     data = preprocessing.scale(preprocessing.normalize(np.array(data, dtype='float32'), copy=False), with_mean=False, copy=False)
-    G, pos = create_data_graph(data)
+    G = create_data_graph(data)
 
-    #G, pos = create_data_graph(read_ids_data('NSL_KDD/20 Percent Training Set.csv'))
+    #G = create_data_graph(read_ids_data('NSL_KDD/20 Percent Training Set.csv'))
     #data = []
     #for key, value in iteritems(pos):
     #    data.append(value)
 
-    grng = GNG(data, surface_graph=G, output_images_dir=output_images_dir)
+    gng = IGNG(data, surface_graph=G, output_images_dir=output_images_dir)
 
     output_gif = 'output.gif'
-    if grng is not None:
-        grng.train(max_iterations=1000)
-        print('Clusters count: {}'.format(grng.number_of_clusters()))
+    if gng is not None:
+        gng.train(max_iterations=20)
+        print('Clusters count: {}'.format(gng.number_of_clusters()))
         convert_images_to_gif(output_images_dir, output_gif)
 
     return 0
